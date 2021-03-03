c9e7bbff8fa500d5ac014af5e75e0b80a775198c
Michel Engelen <jsnerdic@gmail.com>
2021-03-03 08:44:10 +0100

    improved recomposeWithShade function

--- src/utils/colorUtils.ts
+++ src/utils/colorUtils.ts
@@ -160,22 +160,13 @@ const shadeValues: Record<string, number> = {
  * @returns {string} A CSS color string
  */
 export function recomposeWithShade(color: TColorDefinition, shade: string): string {
-    const { values } = color;
-    const h = values[0];
-    const s = values[1] / 100;
-    const l = shadeValues[shade];
-    const a = s * Math.min(l, 1 - l);
-    const f = (n: number, k = (n + h / 30) % 12): number => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-
-    let type = 'rgb';
-    const rgb = [Math.round(f(0) * 255), Math.round(f(8) * 255), Math.round(f(4) * 255)];
-
-    if (color.type === 'hsla') {
-        type += 'a';
-        rgb.push(values[3]);
-    }
-
-    return recomposeColor({ type, values: rgb });
+    const { values, type } = color;
+    const hslValues = [...values];
+    hslValues[0] = values[0];
+    hslValues[1] = values[1] / 100;
+    hslValues[2] = shadeValues[shade];
+    const hslString = `${type}(${hslValues.join(', ')})`
+    return hslToRgb(hslString);
 }
 
 /**
