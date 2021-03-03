2195a7be4d282d8f4ed0856c93392718be2cfed9
Michel Engelen <jsnerdic@gmail.com>
2021-03-03 17:29:50 +0100

    improved props documentation on Shape component

--- src/foundations/shape/Shape.tsx
+++ src/foundations/shape/Shape.tsx
@@ -8,6 +8,8 @@ import { TBorderRadiusSizes, TElevationLevel } from './Shape.types';
 type PShape = {
     /** The border-radius size */
     borderRadius?: TBorderRadiusSizes;
+    /** Should a border be rendered */
+    border?: boolean;
     /**
      * Elevation refers to the z-index depth that an element sits on.
      * The base Elevation is 0 and has no depth.
@@ -15,16 +17,33 @@ type PShape = {
      * In total, there are 6 Elevation levels, not including the base level 0.
      * */
     elevation?: TElevationLevel;
+    /** Which component should be used for rendering the Shape */
     component?: 'div' | 'span' | 'section' | 'aside';
-    width?: number;
-    height?: number;
+    /** set a custom width */
+    width?: number | string | 'auto';
+    /**
+     * set a custom height. When `borderRadius` is set to `'circle'`,
+     * `height` will be set from `width`
+     * */
+    height?: number | string | 'auto';
+    /** set a custom padding */
     padding?: number | number[];
+    /** set a custom background color */
     background?: string;
-    border?: boolean;
     children?: React.ReactNode;
 };
 
-const Shape = ({ border = false, borderRadius = 0, component = 'div', elevation = 0, children, ...rest }: PShape) => {
+const Shape = ({
+    border = false,
+    borderRadius = 0,
+    component = 'div',
+    elevation = 0,
+    background = '#FFF',
+    width = 'auto',
+    height = 'auto',
+    padding = 0,
+    children,
+}: PShape) => {
     const getStyle = (): React.CSSProperties => {
         const style = {
             '--shape-width': 'auto',
@@ -33,31 +52,31 @@ const Shape = ({ border = false, borderRadius = 0, component = 'div', elevation 
             '--shape-background': '#FFF',
         };
 
-        if (rest.width) {
-            style['--shape-width'] = `${rest.width}px`;
+        if (width) {
+            style['--shape-width'] = typeof width === 'number' ? `${width}px` : width;
         }
 
-        if (rest.height) {
-            style['--shape-height'] = `${rest.height}px`;
+        if (height) {
+            style['--shape-height'] = typeof height === 'number' ? `${height}px` : height;
         }
 
         if (borderRadius === 'circle') {
-            style['--shape-height'] = `${rest.width}px`;
+            style['--shape-height'] = style['--shape-width'];
         }
 
-        if (rest.padding) {
-            if (Array.isArray(rest.padding)) {
-                style['--shape-padding'] = rest.padding
+        if (padding) {
+            if (Array.isArray(padding)) {
+                style['--shape-padding'] = padding
                     .slice(0, 4)
                     .map(s => `${s}px`)
                     .join(' ');
             } else {
-                style['--shape-padding'] = `${rest.padding}px`;
+                style['--shape-padding'] = `${padding}px`;
             }
         }
 
-        if (rest.background && isValidColor(rest.background)) {
-            style['--shape-background'] = rest.background;
+        if (background && isValidColor(background)) {
+            style['--shape-background'] = background;
         }
 
         return style as React.CSSProperties;
