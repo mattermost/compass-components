b5236617eba6cf554ba3345f6ceb5fb8c992caf2
Michel Engelen <jsnerdic@gmail.com>
2021-03-04 14:08:06 +0100

    added dependency tables for Shape component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
-export const parameters = {
+import { addParameters } from '@storybook/react';
+
+addParameters({
+    dependencies: {
+        // display only dependencies/dependents that have a story in storybook
+        // by default this is false
+        withStoriesOnly: true,
+
+        // completely hide a dependency/dependents block if it has no elements
+        // by default this is false
+        hideEmpty: true,
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     layout: 'centered',
-};
+});
