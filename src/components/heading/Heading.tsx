8e8a13bdb69aaa9360ff3d21567cad42c12315b2
Dean Whillier <dean@whillier.com>
2021-03-24 10:32:48 -0400

    allow the html element to be derived by default

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -3,38 +3,36 @@ import clsx from 'clsx';
 
 import Typography from '../../foundations/typography/Typography';
 
-import { THeadingElement } from './Heading.types';
 import {
     DEFAULT_HEADING_COLOR,
     DEFAULT_HEADING_MARGIN,
     DEFAULT_HEADING_SIZE,
-    DEFAULT_HEADING_ELEMENT,
     DEFAULT_HEADING_WEIGHT,
     HEADING_ELEMENTS,
 } from './Heading.constants';
 import { PHeading } from './Heading.props';
 
 const Heading: React.FC<PHeading> = ({
-    size = DEFAULT_HEADING_SIZE,
-    element = DEFAULT_HEADING_ELEMENT,
-    weight = DEFAULT_HEADING_WEIGHT,
-    color = DEFAULT_HEADING_COLOR,
-    margin = DEFAULT_HEADING_MARGIN,
     children,
     className,
+    element,
+    color = DEFAULT_HEADING_COLOR,
+    margin = DEFAULT_HEADING_MARGIN,
+    size = DEFAULT_HEADING_SIZE,
+    weight = DEFAULT_HEADING_WEIGHT,
 }): JSX.Element => {
     // Whenever this component is used with an element that is not supported within the headings throw an error!
-    if (!HEADING_ELEMENTS.includes(element)) {
+    if (element && !HEADING_ELEMENTS.includes(element)) {
         throw new Error(
             `Compass Components: Heading component was used with an unsupported element '${element}'.
             Please provide an element from these available options: ${HEADING_ELEMENTS.join(', ')}.`
         );
     }
 
-    let derivedElement: THeadingElement = element;
+    // set the default element based on the defined component size if element is not defined
+    let derivedElement = element;
 
-    // set the default element based on the defined size of element if not defined
-    if (!element) {
+    if (!derivedElement) {
         switch (true) {
             case size === 400:
                 derivedElement = 'h5';
@@ -56,10 +54,12 @@ const Heading: React.FC<PHeading> = ({
         }
     }
 
+    console.log(element, derivedElement);
+
     return (
         <Typography
             className={clsx(className, 'Heading')}
-            element={element || derivedElement}
+            element={derivedElement}
             size={size}
             weight={weight}
             margin={margin}
