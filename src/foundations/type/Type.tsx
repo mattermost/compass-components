cc9df0cb1875ea882ae5f06983bd9135e3900509
Dean Whillier <dean@whillier.com>
2021-03-23 23:28:08 -0400

    foundation type tweaks
    - consolidate font variation style generation
    - set body default weight to regular
    - add in margin defaults

--- src/foundations/type/Type.tsx
+++ src/foundations/type/Type.tsx
@@ -7,29 +7,28 @@ import { BODY_SIZES, DEFAULT_TYPE_SIZE, HEADING_SIZES, HEADING_VARIANTS } from '
 import { PType } from './Type.props';
 import { TTypeSize, TTypeType } from './Type.types';
 
-function generateFontSize(typeSize: TTypeSize, fontType: TTypeType): FlattenSimpleInterpolation {
+function generateFontStyleVariations(
+    typeSize: TTypeSize,
+    fontType: TTypeType
+): FlattenSimpleInterpolation {
     return css`
         &[data-size='${typeSize}'] {
             font-size: var(--${fontType}-font-size-${typeSize});
             line-height: var(--${fontType}-line-height-${typeSize});
-        }
-    `;
-}
 
-function generateMargins(typeSize: TTypeSize, fontType: TTypeType): FlattenSimpleInterpolation {
-    return css`
-        &[data-margin='none'] {
-            margin: 0;
-        }
-        &[data-margin='top'] {
-            margin: var(--${fontType}-margin-top-${typeSize}) 0 0;
-        }
-        &[data-margin='bottom'] {
-            margin: 0 0 var(--${fontType}-margin-bottom-${typeSize});
-        }
-        &[data-margin='both'] {
-            margin: var(--${fontType}-margin-top-${typeSize}) 0
-                var(--${fontType}-margin-bottom-${typeSize});
+            &[data-margin='none'] {
+                margin: 0;
+            }
+            &[data-margin='top'] {
+                margin: var(--${fontType}-margin-top-${typeSize}) 0 0;
+            }
+            &[data-margin='bottom'] {
+                margin: 0 0 var(--${fontType}-margin-bottom-${typeSize});
+            }
+            &[data-margin='both'] {
+                margin: var(--${fontType}-margin-top-${typeSize}) 0
+                    var(--${fontType}-margin-bottom-${typeSize});
+            }
         }
     `;
 }
@@ -55,21 +54,24 @@ const Type = styled.p
             'margin',
         ]),
     })<PType>`
-        // set default styles
+        // set body type styles (default)
+        // - defaults
         font-family: var(--body-font-family);
         font-size: var(--base-font-size);
-        font-weight: var(--font-weight-bold);
+        font-weight: var(--font-weight-regular);
         line-height: var(--base-line-height);
+        margin: var(--body-margin-top-${DEFAULT_TYPE_SIZE}) 0
+            var(--body-margin-bottom-${DEFAULT_TYPE_SIZE});
 
-        // set body font-sizes
-        ${BODY_SIZES.map((bodySize) => generateFontSize(bodySize, 'body'))}
+        // - set font syle variations
+        ${BODY_SIZES.map((bodySize) => generateFontStyleVariations(bodySize, 'body'))}
 
-        // - override weight if specified (body only supports regular and bold)
+        // - override weight if specified (body only supports regular and bold weights)
         &[data-weight='bold'] {
             font-weight: var(--font-weight-bold);
         }
 
-        // specify text colors
+        // - specify text colors
         &[data-color='primary'] {
             color: var(--primary-text-color);
         }
@@ -82,14 +84,12 @@ const Type = styled.p
             color: var(--disabled-text-color);
         }
 
-        // set body-margins
-        ${BODY_SIZES.map((bodySize) => generateMargins(bodySize, 'body'))}
-
-        // set heading styles
+        // set heading type styles
         &[data-type='heading'] {
+            // - default overrides
             font-weight: var(--font-weight-bold);
-
-            ${HEADING_SIZES.map((headingSize) => generateFontSize(headingSize, 'heading'))}
+            margin: var(--heading-margin-top-${DEFAULT_TYPE_SIZE}) 0
+                var(--heading-margin-bottom-${DEFAULT_TYPE_SIZE});
 
             // - headings switch to heading font at size 300
             &[data-size='300'],
@@ -101,6 +101,11 @@ const Type = styled.p
                 font-family: var(--heading-font-family);
             }
 
+            // - set font style variations
+            ${HEADING_SIZES.map((headingSize) =>
+                generateFontStyleVariations(headingSize, 'heading')
+            )}
+
             // - override weight if specified (heading supports all three weights)
             &[data-weight='light'] {
                 font-weight: var(--font-weight-light);
@@ -108,9 +113,6 @@ const Type = styled.p
             &[data-weight='regular'] {
                 font-weight: var(--font-weight-regular);
             }
-
-            // set heading-margins
-            ${HEADING_SIZES.map((headingSize) => generateMargins(headingSize, 'heading'))}
         }
 
         // animation
