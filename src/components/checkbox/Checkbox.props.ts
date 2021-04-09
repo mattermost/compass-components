b7777b2c09ca1a5dc9789a39b8bec8bf75f2a53c
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro.local>
2021-04-09 19:25:03 +0700

    update structure

--- src/components/checkbox/Checkbox.props.ts
+++ src/components/checkbox/Checkbox.props.ts
@@ -1,10 +1,12 @@
 import { TCheckboxSizes, TCheckboxState } from './Checkbox.types';
 
 type PCheckbox = {
-    labelText: string;
-    state: TCheckboxState;
-    size: TCheckboxSizes;
-    hideLabel: boolean;
+    labelText?: string;
+    state?: TCheckboxState;
+    size?: TCheckboxSizes;
+    hideLabel?: boolean;
+    className?: string;
+    borderColor?: string;
 };
 
 export default PCheckbox;
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,32 +1,70 @@
 import React from 'react';
+import styled from 'styled-components';
+import clsx from 'clsx';
 
 import Shape from '../../foundations/shape';
-import Grid, { GridSpacing } from '../../foundations/layout';
-import Text from '../text';
-import Icon from '../icon';
+import Grid, { GridSpacing, TSpacingTokensSymmetric } from '../../foundations/layout';
+import Text, { TTextSize } from '../text';
+import Icon, { TIconSize } from '../icon';
 
 import PCheckbox from './Checkbox.props';
 import { DEFAULT_CHECKBOX_SIZE, DEFAULT_CHECKBOX_STATE } from './Checkbox.constants';
 
-const Checkbox: React.FC<PCheckbox> = ({
+const CheckboxBase: React.FC<PCheckbox> = ({
     labelText = 'Public',
     state = DEFAULT_CHECKBOX_STATE,
     size = DEFAULT_CHECKBOX_SIZE,
     hideLabel = false,
+    className,
 }: PCheckbox): JSX.Element => {
+    let labelSize: TTextSize = 100;
+    let checkboxSize = 20;
+
+    const iconSize: TIconSize = 12;
+
     let background = 'var(--shape-background-color)';
-    let icon = <></>;
+    let borderColor = 'inherit';
+
+    const spacing: TSpacingTokensSymmetric = {
+        vertical: 0,
+        horizontal: 0,
+    };
+
+    switch (size) {
+        case 'lg':
+            labelSize = 200;
+            checkboxSize = 20;
+            spacing.vertical = 25;
+            spacing.horizontal = 25;
+            break;
+        case 'md':
+            labelSize = 75;
+            checkboxSize = 14;
+            spacing.vertical = 0;
+            spacing.horizontal = 0;
+            break;
+        case 'sm':
+            labelSize = 75;
+            checkboxSize = 12;
+            spacing.vertical = 0;
+            spacing.horizontal = 0;
+            break;
+        default:
+            break;
+    }
 
     switch (state) {
         case 'on':
             background = 'var(--primary-color-main, blue)';
-            icon = <Icon glyph="checkbox" />;
+            borderColor = 'var(--primary-color-main, blue)';
             break;
         case 'off':
             background = 'var(--shape-background-color)';
+            borderColor = 'black';
             break;
         case 'status':
-            background = '--shape-background-color';
+            background = 'var(--shape-background-color)';
+            borderColor = 'red';
             break;
         case 'disabled':
             background = 'var(--disabled-text-color)';
@@ -36,36 +74,37 @@ const Checkbox: React.FC<PCheckbox> = ({
     }
 
     return (
-        <Grid
-            row={true}
-            alignment={'center'}
-            justify={'center'}
-            flex={1}
-            padding={GridSpacing.symmetric({ vertical: 0, horizontal: 50 })}
-        >
+        <>
             <Shape
                 borderRadius={4}
                 elevation={1}
                 elevationOnHover={3}
                 background={background}
-                width={size}
+                width={checkboxSize}
+                height={checkboxSize}
             >
                 <Grid
                     alignment={'center'}
                     justify={'center'}
                     flex={1}
-                    padding={GridSpacing.symmetric({ vertical: 0, horizontal: 0 })}
+                    padding={GridSpacing.symmetric(spacing)}
                 >
-                    {icon}
+                    <Icon glyph="check" size={iconSize} />
                 </Grid>
             </Shape>
             {!hideLabel && (
-                <Text element={'p'} size={50} color={'primary'} margin={'none'}>
+                <Text size={labelSize}>
                     <strong>{labelText}</strong>
                 </Text>
             )}
-        </Grid>
+        </>
     );
 };
+
+const Checkbox = styled(CheckboxBase).attrs((props: PCheckbox) => ({
+    className: clsx(props.className, 'Checkbox'),
+}))<PCheckbox>`
+    'color':'white', ;
+`;
 
 export default Checkbox;
