e1e2b6ff4d3b0326b238263b16043b1105ee4fa9
Michel Engelen <jsnerdic@gmail.com>
2021-04-12 16:09:48 +0200

    bumped version of Compass-Icons to add correct typings to it

--- src/components/icon/Icon.constants.ts
+++ src/components/icon/Icon.constants.ts
@@ -24,7 +24,7 @@ const ICON_FONT_SIZES: Record<TIconSize, number> = {
 
 const ICON_GLYPHS: TIconGlyph[] = [
     'none',
-    ...Object.values(IconGlyphs).map((glyph: string) => glyph as TIconGlyph),
+    ...IconGlyphs.map((glyph: string) => glyph as TIconGlyph),
 ];
 
 const DEFAULT_ICON_GLYPH: TIconGlyph = 'mattermost';
--- src/components/icon/Icon.types.ts
+++ src/components/icon/Icon.types.ts
@@ -1,8 +1,8 @@
-import { IconGlyphs } from '@mattermost/compass-icons/build/IconGlyphs';
+import { IconGlyphTypes } from '@mattermost/compass-icons/build/IconGlyphs';
 
 import { TTHemeColors } from '../../foundations/theme-provider/themes/theme.types';
 
-type TIconGlyph = 'none' | typeof IconGlyphs;
+type TIconGlyph = 'none' | IconGlyphTypes;
 
 type TIconSize = 10 | 12 | 14 | 16 | 18 | 20 | 24 | 28 | 32 | 40 | 52 | 64 | 104;
 
