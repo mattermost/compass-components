e966cc6cfbfcccf6530a6393bd345b2b8559dfa1
Michel Engelen <jsnerdic@gmail.com>
2021-05-10 10:21:46 +0200

    improved AvatarBase and added random background color

--- src/components/avatar/Avatar.base.tsx
+++ src/components/avatar/Avatar.base.tsx
@@ -1,6 +1,6 @@
 import Grid from '../../foundations/layout';
 import Shape from '../../foundations/shape';
-import Text from '../text';
+import Heading from '../heading';
 
 import { AVATAR_SIZE_MAP, DEFAULT_AVATAR_SIZE } from './Avatar.constants';
 import PAvatar from './Avatar.props';
@@ -8,9 +8,9 @@ import PAvatar from './Avatar.props';
 const AvatarBase = ({ userName, size = DEFAULT_AVATAR_SIZE, ...rest }: PAvatar): JSX.Element => (
     <Shape borderRadius={'circle'} width={AVATAR_SIZE_MAP[size]} {...rest}>
         <Grid flex={1} alignment={'center'} justify={'center'}>
-            <Text element={'span'} weight={'bold'}>
+            <Heading element={'h6'} weight={'bold'} margin={'none'}>
                 {userName.slice(0, 2)}
-            </Text>
+            </Heading>
         </Grid>
     </Shape>
 );
--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,18 +1,26 @@
 import styled, { css } from 'styled-components';
 import { FlattenSimpleInterpolation, ThemedStyledProps } from 'styled-components/ts3.6';
+import random from 'lodash.random';
 
 import { TTheme } from '../../foundations/theme-provider/themes/theme.types';
+import Heading from '../heading';
 
 import AvatarBase from './Avatar.base';
+import { AVATAR_FALLBACK_COLORS } from './Avatar.constants';
 import PAvatar from './Avatar.props';
 
 const getAvatarStyles = (
-    props: ThemedStyledProps<PAvatar, TTheme>
+    propsIgnore: ThemedStyledProps<PAvatar, TTheme>
 ): FlattenSimpleInterpolation => css`
-    color: ${props.theme.text.primary};
+    color: white;
+    background-color: ${AVATAR_FALLBACK_COLORS[random(0, 7)]};
 `;
 
 const Avatar = styled(AvatarBase)<PAvatar>`
+    ${Heading}::first-letter {
+        text-transform: uppercase;
+    }
+
     ${getAvatarStyles}
 `;
 
