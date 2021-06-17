d18752835541fdd9588ad5b0c07630b9b587344f
Nevyana <nevyana@Nevyanas-MacBook-Pro-2.local>
2021-06-17 13:25:29 +0300

    Final? feedback :D

--- src/components/tag/Tag.props.ts
+++ src/components/tag/Tag.props.ts
@@ -1,10 +1,25 @@
 import { TTagVariant, TTagSizeToken } from './Tag.types';
 
 type PTag = {
+    /**
+     * the variant of the tag for example, mentions, labels and shortcuts
+     */
     variant: TTagVariant;
+    /**
+     * the text rendered inside the tag, for example @username, shortcut
+     */
     text: string;
+    /**
+     * the size of the text rendered in the tag
+     */
     size?: TTagSizeToken;
+    /**
+     * onClick handler
+     */
     onClick?: () => void;
+    /**
+     * custom className
+     */
     className?: string;
 };
 
--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -4,8 +4,14 @@ import { Utils } from '../../shared';
 
 import PTag from './Tag.props';
 import TagRoot from './Tag.root';
+import { DEFAULT_TAG_VARIANT, DEFAULT_TAG_SIZE } from './Tag.constants';
 
-const Tag = ({ variant, size, text, ...rest }: PTag): JSX.Element => {
+const Tag = ({
+    variant = DEFAULT_TAG_VARIANT,
+    size = DEFAULT_TAG_SIZE,
+    text,
+    ...rest
+}: PTag): JSX.Element => {
     const hasText = Utils.isString(text) && text.length > 0;
 
     return (
