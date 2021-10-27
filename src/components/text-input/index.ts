7f2a0dc0999e71e2c1656676787deb19b2b2f2fa
Michel Engelen <jsnerdic@gmail.com>
2021-10-27 17:48:55 +0200

    applied improvements to text and textinput components

--- src/components/text-input/index.ts
+++ src/components/text-input/index.ts
@@ -1,7 +1,5 @@
-import TextInput from './TextInput';
-
 export * from './TextInput.constants';
 export * from './TextInput.props';
 export * from './TextInput.types';
 
-export default TextInput;
+export { default } from './TextInput';
