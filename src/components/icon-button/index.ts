a19d446e4945cd49dab444f8459a08ad7eaca56d
Michel Engelen <jsnerdic@gmail.com>
2021-10-27 17:33:38 +0200

    applied improvements to iconbutton and image components

--- src/components/icon-button/index.ts
+++ src/components/icon-button/index.ts
@@ -1,7 +1,5 @@
-import IconButton from './IconButton';
-
 export * from './IconButton.constants';
 export * from './IconButton.props';
 export * from './IconButton.types';
 
-export default IconButton;
+export { default } from './IconButton';
