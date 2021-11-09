daefedc9bd787fa17bb86ab55363a2feb8ca1b49
Nev Angelova <nevy.angelova@gmail.com>
2021-11-09 16:51:49 +0700

    Update src/patterns/menu/Menu.types.ts
    Co-authored-by: Michel Engelen <32863416+michelengelen@users.noreply.github.com>

--- src/patterns/menu/Menu.types.ts
+++ src/patterns/menu/Menu.types.ts
@@ -8,7 +8,7 @@ import type { TSpacingToken as TMenuOffsetToken } from '../../utilities/spacing'
 type TMenuOffset = [TMenuOffsetToken, TMenuOffsetToken];
 
 type TMenuGroup = {
-    menuItems: typeof MenuItem[];
+    menuItems: MenuItem[];
     title?: string;
 };
 
